2fbbc3619c0afeef0ad65edf6cb25046b749cc9b
rbgo404 <150957746+rbgo404@users.noreply.github.com>
2024-02-28 01:27:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,41 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, pipeline
-import time
+from vllm import LLM, SamplingParams
+from huggingface_hub import snapshot_download
+import os
+from pathlib import Path
 
 class InferlessPythonModel:
     def initialize(self):
-        model_id = "Inferless/inferless-phi-2-DPO"
-        bnb_config = BitsAndBytesConfig(
-            load_in_4bit=True)
-        self.tokenizer = AutoTokenizer.from_pretrained(model_id)
-        model = AutoModelForCausalLM.from_pretrained(model_id, trust_remote_code=True, quantization_config=bnb_config, device_map="cuda")
-        self.pipe = pipeline("text-generation", model=model, tokenizer=self.tokenizer)
+        repo_id = "Inferless/inferless-phi-2-DPO"  # Specify the model repository ID
+        HF_TOKEN = os.getenv("HF_TOKEN")  # Access Hugging Face token from environment variable
+        volume_nfs = "/var/nfs-mount/common_llm"  # Define nfs volume for fast cold-start
+        model_dir = f"{volume_nfs}/{repo_id}"  # Construct model directory path
+        model_dir_path = Path(model_dir)  # Convert path to Path object
 
-    def infer(self, inputs):
-        prompt = inputs["prompt"]
-        messages = [{"role": "system", "content":prompt}]
+        # Create the model directory if it doesn't exist
+        if not model_dir_path.exists():
+            model_dir_path.mkdir(exist_ok=True, parents=True)
 
-        prompt = self.pipe.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
-        out = self.pipe(prompt, max_new_tokens=256, do_sample=True, top_p=0.9,temperature=0.9)
-        generated_text = out[0]["generated_text"][len(prompt):]
-        return {'generated_result': generated_text}
+        # Download the model snapshot from Hugging Face Hub
+        snapshot_download(
+            repo_id,
+            local_dir=model_dir,
+            token=HF_TOKEN  # Provide token if necessary
+        )
+
+        # Define sampling parameters for model generation
+        self.sampling_params = SamplingParams(temperature=0.7, top_p=0.95, max_tokens=256)
+
+        # Initialize the LLM object
+        self.llm = LLM(model=model_dir)
+        
+    def infer(self,inputs):
+        prompts = inputs["prompt"]  # Extract the prompt from the input
+        result = self.llm.generate(prompts, self.sampling_params)
+        # Extract the generated text from the result
+        result_output = [output.outputs[0].text for output in result]
+
+        # Return a dictionary containing the result
+        return {'generated_result': result_output[0]}
 
     def finalize(self):
         pass
